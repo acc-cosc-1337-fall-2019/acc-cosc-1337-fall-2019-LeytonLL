0fc70b5d31f21dd3e17902fab06463c4dae6772f
8584001 <8584001@RRC2220-25>
2019-09-11 19:21:13 -0500

    homework 2 update

--- src/homework/01_expressions/main.cpp
+++ src/homework/01_expressions/main.cpp
@@ -2,7 +2,7 @@
 #include"expressions.cpp"
 #include <iostream>
 //write namespace using statement for cout
-
+using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
