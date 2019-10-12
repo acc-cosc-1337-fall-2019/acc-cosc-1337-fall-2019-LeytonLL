8b7cf4b7b0dbbbf2a60f20bbf6da464ec17276ce
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-11 23:46:23 -0500

    Homework 6 grading.  Missing main.cpp

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,9 +1,43 @@
-#include "catch.hpp"
+#include "tic_tac_toe.h"
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
+	auto response = 'y';
+	TicTacToe board;
+	auto position = 0;
+	string player = "";
 
+	do 
+	{
+		cout << "Enter X or O";
+		cin >> player;
+		board.start_game(player);
 
+		while (!board.game_over()) 
+		{
+			while (position < 1 || position > 9)
+			{
+				cout << "Enter position from 1 to 9\n";
+				cin >> position;
+			}
+
+			board.mark_board(position);
+			position = 0;
+			board.display_board();
+		}
+
+		cout << "Game over: \n";
+		cout << "Continue y or n ";
+		cin >> response;
+
+	
+	} while (response == 'y' || response == 'Y');
 
 
 
