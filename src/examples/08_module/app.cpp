eb70d67ce2b244aed638d7355caef51c0a5eb1ea
8584001 <8584001@RRC2220-25>
2019-11-04 18:59:16 -0600

    classwork

--- src/examples/08_module/app.cpp
+++ src/examples/08_module/app.cpp
@@ -1,1 +1,21 @@
 //app.cpp
+
+#include "app.h"
+
+#include "frame.h"
+
+
+
+bool App::OnInit()
+
+{
+
+	auto frame = new Frame();
+
+	frame->Show(true);
+
+
+
+	return true;
+
+}
--- src/examples/08_module/app.h
+++ src/examples/08_module/app.h
@@ -1,1 +1,23 @@
 //app.h
+
+#ifndef   APP_H
+
+#define APP_H
+
+#include <wx/wx.h>
+
+
+
+class App : public wxApp
+
+{
+
+public:
+
+	virtual bool OnInit();
+
+};
+
+
+
+#endif // ! APP_h
