6ad42cbb7556fe21c7393b3389ee9f5ff60937e5
8584001 <8584001@RRC2220-25>
2019-11-25 18:56:32 -0600

    vector update but also the pain

--- src/classwork/11_assign/vector.cpp
+++ src/classwork/11_assign/vector.cpp
@@ -1,5 +1,14 @@
 #include "vector.h"
+#include "vector.h"
+#include "vector.h"
+#include "vector.h"
+#include "vector.h"
 #include <iostream>
+Vector::Vector()
+	: size{ 0 }, nums{ nullptr }, space{ 0 }
+{
+
+}
 //
 Vector::Vector(size_t sz)
 	:size{sz}, nums{new int[sz]}
@@ -36,6 +45,52 @@ Vector& Vector::operator=(const Vector& v)
 
 }
 
+void Vector::Reserve(size_t new_allocation)
+{
+	if (new_allocation <= space)
+	{
+		return;
+	}
+
+	int* temp = new int[new_allocation];
+
+	for (size_t i = 0; i < size; ++i)
+	{
+		temp[i] = nums[i];
+	}
+
+	delete[] nums;
+	nums = temp;
+
+	space = new_allocation;
+}
+
+
+void Vector::Resize(size_t new_size)
+{
+	Reserve(new_size);
+
+	for (size_t i = 0; i < new_size; ++i)
+	{
+		nums[i] = 0;
+	}
+}
+
+void Vector::Push_Back(int value)
+{
+	if (space == 0)
+	{
+		Reserve(Reserve_Default_Size);
+	}
+	else if (size == space)
+	{
+		Reserve(Reserve_Default_Size * space);
+	}
+
+	nums[size] = value;
+	++size;
+}
+
 Vector::~Vector()
 {
 	std::cout << "Delete Array...";
@@ -52,6 +107,24 @@ Vector::Vector(Vector&& v) // move contrustors
 
 Vector& Vector::operator=(Vector&& v) //move assignment
 {
+	/* if (this == &v) //avoid self copy
+	{
+		return *this;
+	}
+	if (space <= space)
+	{
+		for (size_t i = 0; i < v.size; i++)
+		{
+			nums[i] = v.nums[i];
+		}
+		size = v.size;
+
+		return *this;
+	}*/
+
+	//int* temp  = new int[v.size];
+
+
 	delete nums;
 	nums = v.nums;
 	size = v.size;
@@ -60,3 +133,8 @@ Vector& Vector::operator=(Vector&& v) //move assignment
 
 	return *this;
 }
+
+/*
+
+
+*/
--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -3,16 +3,25 @@
 class Vector
 {
 public:
+	Vector();
 	Vector(size_t sz);
-	Vector(const Vector& v); //copy constructor
-	Vector& operator = (const Vector& v); // copy assignment
+	Vector(const Vector& v); //copy constructor RULE OF THREE
+	Vector& operator = (const Vector& v); // copy assignment RULE OF THREE
 	Vector(Vector&& v); // move contrustors
 	Vector& operator =(Vector&& v); //move assignment
 	size_t Size()const { return size; }
 	int& operator[] (int i){ return nums[i]; }
 	int& operator[] (int i) const { return nums[i]; }
-	~Vector();
+	void Reserve(size_t new_allocation);
+	int Capacity() const { return space; } //define inline
+	void Resize(size_t new_size);
+	void Push_Back(int value);
+	~Vector(); //desconstructor RULE OF THREE
 private:
+
 	size_t size;
+	size_t space;
 	int* nums;
+	const int Reserve_Default_Size{ 8 };
+	const int RESERVE_SPACE_MUITIPLIER{ 2 };
 };
